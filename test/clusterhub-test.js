80b88ea260067c79949a3e0df7d16e0bb17434a1
Roly Fentanes <roly426@gmail.com>
2013-03-19 17:01:36 -0700

    use sinon to fake time for faster tests

--- test/clusterhub-test.js
+++ test/clusterhub-test.js
@@ -1,80 +1,69 @@
+/*jshint expr:true */
+var HubStore = require('..');
+var should = require('should');
+var sinon = require('sinon');
 
-/**
- * Test dependencies
- *
- * @api private
- */
 
-var HubStore = require('..')
-  , should = require('should')
+exports['test publishing doesnt get caught by the own store subscriber'] = function(done) {
+  var a = new HubStore(1);
 
-/**
- * Test.
- */
-
-module.exports = {
-
-  'test publishing doesnt get caught by the own store subscriber': function (done) {
-    var a = new HubStore(1)
-
-    a.subscribe('woot', function (arg) {
-      arg.should.equal('bb');
-      a.destroy();
-      b.destroy();
-      done();
-    }, function () {
-      a.publish('woot', 'aa');
-    });
-
+  a.subscribe('woot', function (arg) {
+    arg.should.equal('bb');
+    a.destroy();
     done();
-  },
+  }, function () {
+    a.publish('woot', 'aa');
+  });
 
-  'test storing data for a client': function (done) {
-    var store = new HubStore
-      , rand = 'test-' + Date.now()
-      , client = store.client(rand);
+  done();
+};
 
-    client.id.should.equal(rand);
 
-    client.set('a', 'b', function (err) {
+exports['test storing data for a client'] = function(done) {
+  var store = new HubStore()
+    , rand = 'test-' + Date.now()
+    , client = store.client(rand);
+
+  client.id.should.equal(rand);
+
+  client.set('a', 'b', function (err) {
+    should.strictEqual(err, null);
+
+    client.get('a', function (err, val) {
       should.strictEqual(err, null);
+      val.should.equal('b');
 
-      client.get('a', function (err, val) {
+      client.has('a', function (err, has) {
         should.strictEqual(err, null);
-        val.should.equal('b');
+        has.should.be.true;
 
-        client.has('a', function (err, has) {
+        client.has('b', function (err, has) {
           should.strictEqual(err, null);
-          has.should.be.true;
+          has.should.be.false;
 
-          client.has('b', function (err, has) {
+          client.del('a', function (err) {
             should.strictEqual(err, null);
-            has.should.be.false;
 
-            client.del('a', function (err) {
+            client.has('a', function (err, has) {
               should.strictEqual(err, null);
+              has.should.be.false;
 
-              client.has('a', function (err, has) {
+              client.set('b', 'c', function (err) {
                 should.strictEqual(err, null);
-                has.should.be.false;
 
-                client.set('b', 'c', function (err) {
+                client.set('c', 'd', function (err) {
                   should.strictEqual(err, null);
 
-                  client.set('c', 'd', function (err) {
+                  client.get('b', function (err, val) {
                     should.strictEqual(err, null);
+                    val.should.equal('c');
 
-                    client.get('b', function (err, val) {
+                    client.get('c', function (err, val) {
                       should.strictEqual(err, null);
-                      val.should.equal('c');
+                      val.should.equal('d');
 
-                      client.get('c', function (err, val) {
-                        should.strictEqual(err, null);
-                        val.should.equal('d');
-
-                        store.destroy();
-                        done();
-                      });
+                      store.destroy();
+                      done();
                     });
                   });
                 });
@@ -84,122 +73,124 @@ module.exports = {
         });
       });
     });
-  },
+  });
+};
 
-  'test cleaning up clients data': function (done) {
-    var rand1 = Math.abs(Math.random() * Date.now() | 0)
-      , rand2 = Math.abs(Math.random() * Date.now() | 0);
 
-    var store = new HubStore()
-      , client1 = store.client(rand1)
-      , client2 = store.client(rand2);
+exports['test cleaning up clients data'] = function(done) {
+  var rand1 = Math.abs(Math.random() * Date.now() | 0)
+    , rand2 = Math.abs(Math.random() * Date.now() | 0);
 
-    client1.set('a', 'b', function (err) {
+  var store = new HubStore()
+    , client1 = store.client(rand1)
+    , client2 = store.client(rand2);
+
+  client1.set('a', 'b', function (err) {
+    should.strictEqual(err, null);
+
+    client2.set('c', 'd', function (err) {
       should.strictEqual(err, null);
 
-      client2.set('c', 'd', function (err) {
+      client1.has('a', function (err, val) {
         should.strictEqual(err, null);
+        val.should.be.true;
 
-        client1.has('a', function (err, val) {
+        client2.has('c', function (err, val) {
           should.strictEqual(err, null);
           val.should.be.true;
 
-          client2.has('c', function (err, val) {
+          store.destroy();
+
+          var newstore = new HubStore()
+            , newclient1 = newstore.client(rand1)
+            , newclient2 = newstore.client(rand2);
+
+          newclient1.has('a', function (err, val) {
             should.strictEqual(err, null);
-            val.should.be.true;
+            val.should.be.false;
 
-            store.destroy();
-
-            var newstore = new HubStore()
-              , newclient1 = newstore.client(rand1)
-              , newclient2 = newstore.client(rand2);
-
-            newclient1.has('a', function (err, val) {
+            newclient2.has('c', function (err, val) {
               should.strictEqual(err, null);
               val.should.be.false;
 
-              newclient2.has('c', function (err, val) {
-                should.strictEqual(err, null);
-                val.should.be.false;
-
-                newstore.destroy();
-                done();
-              });
-            });
-          });
-        });
-      });
-    });
-  },
-
-  'test cleaning up a particular client': function (done) {
-    var rand1 = Math.abs(Math.random() * Date.now() | 0)
-      , rand2 = Math.abs(Math.random() * Date.now() | 0);
-
-    var store = new HubStore()
-      , client1 = store.client(rand1)
-      , client2 = store.client(rand2);
-
-    client1.set('a', 'b', function (err) {
-      should.strictEqual(err, null);
-
-      client2.set('c', 'd', function (err) {
-        should.strictEqual(err, null);
-
-        client1.has('a', function (err, val) {
-          should.strictEqual(err, null);
-          val.should.be.true;
-
-          client2.has('c', function (err, val) {
-            should.strictEqual(err, null);
-            val.should.be.true;
-
-            store.clients.should.have.property(rand1);
-            store.clients.should.have.property(rand2);
-            store.destroyClient(rand1);
-
-            store.clients.should.not.have.property(rand1);
-            store.clients.should.have.property(rand2);
-
-            client1.has('a', function (err, val) {
-              should.strictEqual(err, null);
-              val.should.equal(false);
-
-              store.destroy();
+              newstore.destroy();
               done();
             });
           });
         });
       });
     });
-  },
+  });
+};
 
-  'test destroy expiration': function (done) {
-    var store = new HubStore()
-      , id = Math.abs(Math.random() * Date.now() | 0)
-      , client = store.client(id);
 
-    client.set('a', 'b', function (err) {
+exports['test cleaning up a particular client'] = function(done) {
+  var rand1 = Math.abs(Math.random() * Date.now() | 0)
+    , rand2 = Math.abs(Math.random() * Date.now() | 0);
+
+  var store = new HubStore()
+    , client1 = store.client(rand1)
+    , client2 = store.client(rand2);
+
+  client1.set('a', 'b', function (err) {
+    should.strictEqual(err, null);
+
+    client2.set('c', 'd', function (err) {
       should.strictEqual(err, null);
-      store.destroyClient(id, 1);
 
-      setTimeout(function () {
-        client.get('a', function (err, val) {
+      client1.has('a', function (err, val) {
+        should.strictEqual(err, null);
+        val.should.be.true;
+
+        client2.has('c', function (err, val) {
           should.strictEqual(err, null);
-          val.should.equal('b');
-        });
-      }, 500);
+          val.should.be.true;
 
-      setTimeout(function () {
-        client.get('a', function (err, val) {
-          should.strictEqual(err, null);
-          should.strictEqual(val, null);
+          store.clients.should.have.property(rand1);
+          store.clients.should.have.property(rand2);
+          store.destroyClient(rand1);
 
-          store.destroy();
-          done();
+          store.clients.should.not.have.property(rand1);
+          store.clients.should.have.property(rand2);
+
+          client1.has('a', function (err, val) {
+            should.strictEqual(err, null);
+            val.should.equal(false);
+
+            store.destroy();
+            done();
+          });
         });
-      }, 1500);
+      });
     });
-  }
+  });
+};
 
+
+exports['test destroy expiration'] = function(done) {
+  var store = new HubStore()
+    , id = Math.abs(Math.random() * Date.now() | 0)
+    , client = store.client(id)
+    , clock = sinon.useFakeTimers();
+
+  client.set('a', 'b', function (err) {
+    should.strictEqual(err, null);
+    store.destroyClient(id, 1);
+
+    clock.tick(500);
+    client.get('a', function (err, val) {
+      should.strictEqual(err, null);
+      val.should.equal('b');
+    });
+
+    clock.tick(1500);
+    client.get('a', function (err, val) {
+      should.strictEqual(err, null);
+      should.strictEqual(val, null);
+
+      store.destroy();
+      done();
+    });
+
+  });
 };
